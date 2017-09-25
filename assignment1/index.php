56c8126e71148b434b40aac380a2fa8d691f7493
aqukandy <aqukandy@gmail.com>
2017-09-25 00:23:49 -0400

    added

---
+++ assignment1/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE HTML>
+
+<?php
+session_start();
+
+$username = "abadee";
+$password = "123456";
+
+$errir = "";
+
+if (isset($_SESSION['loggedIn']) && $_SESSION['loggedIn'] == true) {
+    $error = "success";
+    header("Location: success.php");
+}
+
+if (isset($_POST['username']) && isset($_POST['password'])) {
+    if ($_POST['username'] == $username && $_POST['password'] == $password) {
+        $_SESSION['loggedIn'] = true;
+        header('Location: success.php');
+    } else {
+        $_SESSION['loggedIn'] = false;
+        $error = "Invalid username and password!";
+    }
+}
+?>
+
+
+    <head>
+        <title>Login page</title>
+    </head>
+
+    
+        <!-- output error massage if any -->
+        <?php echo $error; ?>
+
+        <!-- from for login -->
+        <form method ="post" action="index.php">
+            <label for ="Username">Username:</label><br/>
+            <input type="text" name="username" id="username"><br/>
+            <label for ="password">password:</label><br/>
+            <input type="password" name="password" id="password"><br/>
+            <input type="submit" value="Log In">
+        </form>
+    
+
---
+++ assignment1/success.php
@@ -0,0 +1,14 @@
+<?php
+ob_start();
+session_start();
+
+if (!isset($_SESSION['loggedIn']) || $_SESSION['loggedIn'] == false) {
+    header("Location: index.php");
+}
+?>
+<h1> Logged In!</h1>
+<form method ="post" action="index.php">
+    <input type="submit" value="Logout">
+</form>
+
+
