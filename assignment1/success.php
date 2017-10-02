884582483a63f71c112b8840a50e1c737fb01f6a
aqukandy <aqukandy@gmail.com>
2017-10-02 00:16:54 -0400

    added
    added the username and the password to the logged in page

--- assignment1/success.php
+++ assignment1/success.php
@@ -1,13 +1,10 @@
 <?php
 ob_start();
 session_start();
+echo "Logged In! " . $_SESSION['username'] . " password is " . $_SESSION['password'] . " date and time " . date("Y/m/d") . " " . date("H/i/s");
 
-if (!isset($_SESSION['loggedIn']) || $_SESSION['loggedIn'] == false) {
-    header("Location: index.php");
-}
 ?>
-<h1> Logged In!</h1>
-<form method ="post" action="index.php">
+<form method ="post" action="logout.php">
     <input type="submit" value="Logout">
 </form>
 
