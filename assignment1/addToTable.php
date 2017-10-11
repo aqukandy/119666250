cc46927f9528a91d911bc7a2c55f5af20899ba28
aqukandy <aqukandy@gmail.com>
2017-10-10 23:21:20 -0400

    created class
    created a new class called addToTable, to add the user informations to the database

---
+++ assignment1/addToTable.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "cosc";
+
+$email = $_POST['Email'];
+$user = $_POST['Username'];
+$pass1 = $_POST['Password'];
+
+
+
+
+
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $sql = "INSERT INTO `users`(`Email`, `Username`, `Password`)
+			VALUES ('$email','$user','$pass1')";
+
+
+
+    // use exec() because no results are returned
+    $conn->exec($sql);
+    echo "New record created successfully";
+} catch (PDOException $e) {
+    echo $sql . "<br>" . $e->getMessage();
+}
+?>
