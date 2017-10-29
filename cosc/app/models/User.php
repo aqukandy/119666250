4a68c38ad2bc57467d4d97950ea310ebba4ad04f
aqukandy <aqukandy@gmail.com>
2017-10-29 01:17:18 -0400

    fixed code
    fixed code user class

--- cosc/app/models/User.php
+++ cosc/app/models/User.php
@@ -1,44 +1,58 @@
-<?php
+d<?php
 
 class User {
 
     public $username;
     public $password;
     public $auth = false;
-
+    public $email;
+    public $time;
+    public $attempt;
     public function __construct() {
         
     }
 
     public function authenticate() {
-        /*
-         * if username and password good then
-         * $this->auth = true;
-         */
-		 
-		$db = db_connect();
-        $statement = $db->prepare("select * from users
-                                WHERE username = :username;
-                ");
+        	 
+        $db = db_connect();
+        $statement = $db->prepare("select Username, Password from users WHERE Username = :name;");
         $statement->bindValue(':name', $this->username);
         $statement->execute();
         $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
-		
-		if ($rows) {
-			$this->auth = true;
-			$_SESSION['username'] = $rows[0]['username'];
-			$_SESSION['password'] = $rows[0]['password'];
-		}
+        $hash_pwd = $rows[0]['Password'];
+        $password = $this->password;
+
+        if (!password_verify($password,$hash_pwd)){
+           $attempt = $attempt + 1;
+           $_SESSION['report'] = $_SESSION['report'] + 1;
+           $db = db_connect();		
+           $statement = $db->prepare("INSERT INTO log (Username, Attempt, Time)"
+                    . "VALUES (:username, :attempt, :time ); ");
+           $statement->bindValue(':username',$this->$username);
+           $statement->bindValue(':attempts',$this->$attempt);
+           $statement->bindValue(':time',time());
+           $statement->execute();
+        } else {
+           $this->auth = true;
+           $_SESSION['username'] = $rows[0]['Username'];
+           $_SESSION['password'] = $rows[0]['Password'];
+               }
+
     }
-	
-	public function register ($username, $password) {
-		$db = db_connect();
-        $statement = $db->prepare("INSERT INTO users (username)"
-                . " VALUES (:username); ");
 
-        $statement->bindValue(':username', $username);
-        $statement->execute();
-
-	}
-
-}
+    public function register ($email, $username, $password) {
+        if(strlen($password) >= 3){
+           $hashPass = password_hash($password, PASSWORD_DEFAULT);     
+           $db = db_connect();
+           $statement = $db->prepare("INSERT INTO users (Email, Username, Password)"
+                    . " VALUES (:email, :username, :password); ");
+           $statement->bindValue(':email', $email);
+           $statement->bindValue(':username', $username);
+           $statement->bindValue(':password', $password);
+           $statement->execute();
+           $_SESSION['auth'] = true;
+        } else {
+            header('Location: /login/register');
+               }
+    }
+}  
