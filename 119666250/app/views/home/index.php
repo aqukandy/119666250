3f3cd32221853d2adc9bdb4eed755d9a5e79706d
aqukandy <aqukandy@gmail.com>
2017-11-06 21:15:02 -0500

    remove index from model and added to view

--- 119666250/app/views/home/index.php
+++ 119666250/app/views/home/index.php
@@ -1,20 +1,21 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    </head>
-    <body>
-        <h1 style="font-size:10vw;">Welcome</h1>
-        <?php
-        echo "WELCOME TO MY WEBSITE! " . $_SESSION['username'] . $_SESSION['report'];
-        echo "  Today is: " . date("F jS, Y") . " and The Time is " . date("h:i:sa");
-        ?>
-        <form action="<?= REMIND ?>" method="post">
-            <input type="submit" value="ADD"/>
-        </form>
-        <a href="<?= REMIND ?>">Add Reminder</a>
-        <a href="/logout"> logout! </a>
-    </body>
-</html>
+<?php require_once '../app/views/templates/header.php' ?>
 
+<h2>Reminder Page</h2>
 
+<table class='table table-striped table-condensed'>
+    <tr>
+        <th>Subject</th>
+        <th>Action</th>
+    </tr>
+
+    <?php foreach ($data['list'] as $items) { ?>
+        <tr>
+            <td><?= $items['subject'] ?></td>
+            <td><a href="/remind/remove/<?= $items['id'] ?>">Remove</a> | 
+                <a href="/remind/<?= $items['id'] ?>">View</a> | 
+                <a href="/remind/update/<?= $items['id'] ?>">Update</a>
+            </td>
+        </tr>
+    <?php } ?>
+
+</table>
